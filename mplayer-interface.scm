220e62124b58650ae7982b4f25bab0de4e1baa58
Thomas Hintz <t@thintz.com>
2012-04-04 15:32:29 -0700

    basic commands, use fifo in home directory

--- mplayer-interface.scm
+++ mplayer-interface.scm
@@ -1,12 +1,23 @@
 (use posix)
 
+(define (mplayer-command command)
+  (process-wait (process-run (++ "echo \"" command "\" > ~/.mplayer-out"))))
+
 (define (mplayer-load path)
   (let-values
       (((in out pid)
-	(process (string-append "mplayer -slave -identify -noquiet -input file=cmds " path))))
-    (port-for-each (lambda (v)
-		     (if (string=? v "ID_EXIT=QUIT")
-			 (process-wait pid)
-			 #f))
-		   (lambda ()
-		     (read-line in)))))
+	(process (string-append "mplayer -slave -identify -noquiet -input file=~/.mplayer-out \"" path "\""))))
+    (thread-start!
+     (make-thread
+      (lambda ()
+	(port-for-each (lambda (v)
+			 (if (string=? v "ID_EXIT=QUIT")
+			     (process-wait pid)
+			     #f))
+		       (lambda ()
+			 (read-line in))))))))
+
+(define (mplayer-stop)
+  (mplayer-command "quit"))
+(define (mplayer-pause)
+  (mplayer-command "pause"))
